d51b9c261a2943805be5119f35ca2ec757c0b908
mayliz95 <mayra.rosero@epn.edu.ec>
2018-04-17 15:08:11 -0500

    Variables y pruebas con booleanos

---
+++ 01-tipos-variables/tipos.js
@@ -0,0 +1,23 @@
+var nombre = "22"
+var edad = 22
+var fecha = new Date();
+var casado = false; // true
+var casa = undefined;  //Undefined es falso
+var carro = null;
+var arreglo = []
+
+if (nombre !== edad) {
+
+    console.log("V")
+} else {
+    console.log("F")
+}
+
+console.log(fecha)
+
+console.log(casa?"Verdadero": "Falso")
+
+console.log(-1?"Verdadero": "Falso")
+console.log(0?"Verdadero": "Falso")
+console.log(1?"Verdadero": "Falso")
+console.log(arreglo?"Verdadero": "Falso")
